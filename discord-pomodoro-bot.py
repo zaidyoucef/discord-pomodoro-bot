616ed956a44472a318bc4b1c1b3070fc2a04264c
Youcef Zaid <102479929+zaidyoucef@users.noreply.github.com>
2023-02-19 19:21:56 +0100

    added new command and comments to the code

--- discord-pomodoro-bot.py
+++ discord-pomodoro-bot.py
@@ -1,26 +1,32 @@
-# Bot that allows you to start pomodoro sessions of 25 minutes with a 5 minutes break and push a notification when it ends.
+# Author: @youcefzaid
+# Description: A discord bot that helps you study using the pomodoro technique
 
-import asyncio
+# import the required libraries and modules
+from dataclasses import dataclass
+from discord.ext import commands
 import asyncio
 import datetime
-from discord.ext import commands
-from dataclasses import dataclass
 import discord
 
+# Bot token and channel id. You can get the bot token from the discord developer portal and the channel id from the discord client
 bot_token = "BOT_TOKEN"
 channel_id = CHENNEL_ID
 
 
+# Dataclass to store the session data
 @dataclass
 class Session:
     is_active: bool = False
     start_time: int = 0
+    completed_sessions: int = 0
 
 
+# Create the bot and the session
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 session = Session()
 
 
+# When the bot is ready, send a message to the channel
 @bot.event
 async def on_ready():
     print("Hello! Study bot is ready!")
@@ -28,13 +34,12 @@ async def on_ready():
     await channel.send("Hello! Study bot is ready!")
 
 
+# starts a session
 @bot.command()
 async def start(ctx, sessions: int = 1):
-
     if session.is_active:
         await ctx.send("A session is already active!")
         return
-
     for i in range(sessions):
         session.start_time = datetime.datetime.now()
         session.is_active = True
@@ -42,12 +47,22 @@ async def start(ctx, sessions: int = 1):
 
         await asyncio.sleep(25 * 60)
         await ctx.send("Session ended!")
+        completed_sessions = + 1
         session.is_active = False
         await ctx.send("Take a 5 minutes break!")
         await asyncio.sleep(5 * 60)
         await ctx.send("Break ended!")
 
+    await ctx.send("All sessions completed!")
 
+
+# returns the number of completed sessions
+@bot.command()
+async def completed(ctx):
+    await ctx.send(f"Completed sessions: {session.completed_sessions}")
+
+
+# ends the session
 @bot.command()
 async def end(ctx):
     if not session.is_active:
@@ -58,6 +73,40 @@ async def end(ctx):
     await ctx.send("Session ended!")
 
 
+# pauses the session
+@bot.command()
+async def pause(ctx):
+    if not session.is_active:
+        await ctx.send("No session is active!")
+        return
+
+    session.is_active = False
+    await ctx.send("Session paused!")
+
+
+# resumes the session
+@bot.command()
+async def resume(ctx):
+    if session.is_active:
+        await ctx.send("A session is already active!")
+        return
+
+    session.is_active = True
+    await ctx.send("Session resumed!")
+
+
+# returns the time elapsed since the start of the session
+@bot.command()
+async def elapsed(ctx):
+    if not session.is_active:
+        await ctx.send("No session is active!")
+        return
+
+    time_elapsed = datetime.datetime.now() - session.start_time
+    await ctx.send(f"Time elapsed: {time_elapsed}")
+
+
+# returns the status of the session
 @bot.command()
 async def status(ctx):
     if session.is_active:
@@ -65,5 +114,5 @@ async def status(ctx):
     else:
         await ctx.send("No session is active!")
 
-
+# Run the bot
 bot.run(bot_token)
